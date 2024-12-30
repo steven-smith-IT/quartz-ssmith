df71a94df93cdd560deb7a38bb5a3f569e956165
Steven Smith <steven@stevensmith.me>
2024-12-30 14:17:27 -0500

    Quartz sync: Dec 30, 2024, 2:17 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'steven-smith-IT/quartz-ssmith',
+        // from data-repo-id
+        repoId: 'R_kgDONh7DmQ',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDONh7Dmc4ClnaB',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {},
   }),
